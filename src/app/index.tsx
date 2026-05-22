be8a47e57f82627cafab7e7288b6b1642c164b63
lulultfh <punyalulu05@gmail.com>
2026-05-22 20:31:02 +0700

    feat: menambahkan const untuk email dan password dan state untuk mengubah valuenya, menambahkan function handleSubmit, menambahkan container yang berisikan form input dan button untuk submit, serta styling untuk container dan inputan di dalamnya

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,59 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function Index() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = () => {
+    if (!email || !password) {
+      Alert.alert("error", "Semua kolom wajib diisi!");
+      return;
+    }
+    Alert.alert(
+      "Sukses",
+      `Data terkirim!\n\nEmail: ${email}\nPassword: ${password}`,
+    );
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Selamat Datang</Text>
       <Text style={styles.subTitle}>Silakan masuk ke akun anda</Text>
+      <View style={styles.containerForm}>
+        <View style={styles.inputGroup}>
+          <Text style={styles.label}>Username/Email</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Masukkan username/email anda"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+            // secureTextEntry={true}
+            autoCapitalize="none"
+          />
+        </View>
+        <View style={styles.inputGroup}>
+          <Text style={styles.label}>Password</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Masukkan password anda"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            secureTextEntry={true}
+            autoCapitalize="none"
+          />
+        </View>
+        <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+          <Text style={styles.buttonText}>Submit</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -12,7 +61,7 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // alignItems: "center",
+    backgroundColor: "#F0E9B6",
     justifyContent: "center",
   },
   title: {
@@ -25,5 +74,42 @@ const styles = StyleSheet.create({
     color: "#744577",
     fontSize: 15,
     marginHorizontal: 20,
+  },
+  containerForm: {
+    backgroundColor: "#F2EAE0",
+    borderRadius: 20,
+    shadowColor: "#744577",
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 3,
+    padding: 20,
+    margin: 20,
+  },
+  inputGroup: {
+    marginBottom: 20,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 8,
+    fontWeight: "600",
+    color: "#744577",
+  },
+  input: {
+    paddingVertical: 12,
+    fontSize: 16,
+    borderBottomColor: "#744577",
+    borderBottomWidth: 1,
+  },
+  button: {
+    backgroundColor: "#744577",
+    paddingVertical: 14,
+    borderRadius: 8,
+    marginTop: 10,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
