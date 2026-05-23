3c58a7e44b6f904fc0463027165cc05d36d4119b
lulultfh <punyalulu05@gmail.com>
2026-05-23 09:18:17 +0700

    feat: menambahkan form section untuk isi data flight

--- src/app/dashboard.tsx
+++ src/app/dashboard.tsx
@@ -1,12 +1,74 @@
 import { useLocalSearchParams } from "expo-router";
 import { useState } from "react";
-import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  Alert,
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
+import {
+  DateTimePickerAndroid,
+  DateTimePickerEvent,
+} from "@react-native-community/datetimepicker";
 
 export default function home() {
   const { email } = useLocalSearchParams();
+
+  const [lokasi, setLokasi] = useState("");
+  const [destinasi, setDestinasi] = useState("");
+  const [departure, setDeparture] = useState<Date>(new Date());
+  const [kembali, setKembali] = useState<Date>(new Date());
+  const [showDeparturePicker, setShowDeparturePicker] = useState(false);
+  const [showReturnPicker, setShowReturnPicker] = useState(false);
+
+  const formatDate = (date: Date) => {
+    return date.toLocaleDateString("en-US", {
+      month: "short",
+      day: "numeric",
+      year: "numeric",
+    });
+  };
+
+  const openDeparturePicker = () => {
+    DateTimePickerAndroid.open({
+      value: departure,
+      onChange: (event: DateTimePickerEvent, selectedDate?: Date) => {
+        if (selectedDate) setDeparture(selectedDate);
+      },
+      mode: "date",
+      minimumDate: new Date(), // Tidak bisa pilih hari kemarin
+    });
+  };
+
+  const openReturnPicker = () => {
+    DateTimePickerAndroid.open({
+      value: kembali,
+      onChange: (event: DateTimePickerEvent, selectedDate?: Date) => {
+        if (selectedDate) setKembali(selectedDate);
+      },
+      mode: "date",
+      minimumDate: departure, // Tidak bisa sebelum tanggal berangkat
+    });
+  };
+
   const [selectedTrip, setSelectedTrip] = useState<"round" | "one" | "multi">(
     "round",
   );
+
+  const handleSubmit = () => {
+    if (!lokasi || !destinasi || !departure || !kembali) {
+      Alert.alert("error", "Semua kolom wajib diisi!");
+      return;
+    }
+    Alert.alert(
+      "Sukses",
+      `Data terkirim!\n\nLokasi: ${lokasi}\nDestinasi: ${destinasi}\nDeparture: ${departure}\nKembali: ${kembali}`,
+    );
+  };
   return (
     <View style={styles.container}>
       <View style={styles.hero}>
@@ -71,6 +133,54 @@ export default function home() {
           </TouchableOpacity>
         </View>
       </View>
+      <View style={styles.form}>
+        <View style={styles.inputGroup}>
+          <Text style={styles.label}>From (Location)</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Masukkan lokasi anda"
+            value={lokasi}
+            onChangeText={(text) => setLokasi(text)}
+            // secureTextEntry={true}
+            autoCapitalize="none"
+          />
+        </View>
+        <View style={styles.inputGroup}>
+          <Text style={styles.label}>To (Destination)</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Masukkan destinasi yang ingin anda tuju"
+            value={destinasi}
+            onChangeText={(text) => setDestinasi(text)}
+            // secureTextEntry={true}
+            autoCapitalize="none"
+          />
+        </View>
+        <View style={styles.row2}>
+          <View style={styles.inputRow}>
+            <Text style={styles.label}>Departure</Text>
+            <TouchableOpacity
+              style={styles.inputButton}
+              onPress={openDeparturePicker}
+            >
+              <Text style={styles.input}>{formatDate(departure)}</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.inputRow}>
+            <Text style={styles.label}>Return</Text>
+            <TouchableOpacity
+              style={styles.inputButton}
+              onPress={openReturnPicker}
+            >
+              <Text style={styles.input}>{formatDate(kembali)}</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+          <Text style={styles.buttonText}>Submit</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -94,6 +204,13 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-between",
     marginHorizontal: 20,
+  },
+  row2: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    gap: 12,
+    // marginHorizontal: 20,
   },
   textWrap: {
     flexDirection: "column",
@@ -120,7 +237,7 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     padding: 4,
     marginHorizontal: 20,
-    marginBottom: 10,
+    marginBottom: 20,
   },
   tabButton: {
     flex: 1,
@@ -131,4 +248,59 @@ const styles = StyleSheet.create({
   activeTabButton: { backgroundColor: "#744577" },
   tabText: { color: "#744577", fontWeight: "600" },
   activeTabText: { color: "#fff" },
+  form: {
+    backgroundColor: "#BDA6CE",
+    maxWidth: "100%",
+    marginHorizontal: 24,
+    marginTop: -10,
+    borderRadius: 24,
+    padding: 20,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: 0.1,
+    shadowRadius: 15,
+    elevation: 8,
+  },
+  inputGroup: {
+    marginBottom: 20,
+  },
+  inputButton: {
+    backgroundColor: "#F2EAE0",
+    padding: 14,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: "#ddd",
+  },
+  inputRow: {
+    flex: 1,
+    // marginBottom: 20,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 8,
+    fontWeight: "600",
+    color: "#744577",
+  },
+  input: {
+    backgroundColor: "#F2EAE0",
+    opacity: 0.8,
+    paddingHorizontal: 15,
+    paddingVertical: 12,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    fontSize: 16,
+  },
+  button: {
+    backgroundColor: "#744577",
+    paddingVertical: 14,
+    borderRadius: 20,
+    marginTop: 10,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
 });
